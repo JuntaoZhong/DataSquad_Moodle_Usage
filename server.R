1431763cd2d8bfb708b73807dfd0725943dd85f3
zhongj2 <zhongj2@maize.mathcs.carleton.edu>
2021-03-23 12:34:27 -0500

    the plot counts part is working now

--- server.R
+++ server.R
@@ -9,20 +9,19 @@
 
 library(shiny)
 
+# reference Hiro's R script functions
+source('module_bar.R', local = TRUE)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
     output$userInputAll <- renderUI({
-            str1 <- paste("you selected dates", input$dates[1], input$dates[2])
-            str2 <- paste("you selected ", input$selectedDepartment, ", and", input$selectedTeachMode)
-            str3 <- paste("<br> currently waiting for the real R code")
-            HTML(paste(str1, str2, str3, sep = '<br/>'))
+            str1 <- paste("you selected time", input$selectedYear, input$selectedTerm)
+            str2 <- paste("and you selected ", input$selectedDepartment)
+            HTML(paste(str1, str2, sep = '<br/>'))
         }
     )
+    output$barChart <- renderPlot({
+        termYear <- paste(toString(input$selectedYear), input$selectedTerm, sep = "")
+        plot_counts(termYear, div = 'All', subj = input$selectedDepartment)
+    })
 })
--- ui.R
+++ ui.R
@@ -11,39 +11,24 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+
+courseVector <- unlist(read.csv("./sb21_division_lookup.csv")[,"fullname"])
+
 shinyUI(fluidPage(
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    ),
-    fluidRow(column(3, dateRangeInput("dates", h3("Date range"))),
-             
-             column(3, radioButtons("selectedDepartment", h3("Department"),
-                                    choices = list("all", "Biology", "Spanish"),
-                                    selected = "all")),
-             
-             column(3, 
-                    selectInput("selectedYear", h3("year"), 
-                                   choices = c(2014:2020), 
-                                   selected = "2020"),
-                    selectInput("term", h3("term"), 
-                                choices = c("Fall", "Winter", "Spring"), 
-                                selected = "Spring")),
+    titlePanel("Moodle Usage Visualization"),
+    mainPanel(plotOutput("barChart",height = 500)),
+    fluidRow(column(3, selectInput("selectedDepartment", h3("Department"),
+                                choices = courseVector,
+                                selected = "Mathematics"),
+                    
+                    # split sub-column in half
+                    column(6, selectInput("selectedYear", h3("year"), 
+                                          choices = c(2014:2020), 
+                                          selected = "2020")),
+                    column(6, radioButtons("selectedTerm", h3("term"), 
+                                           choices = c("Fall", "Winter", "Spring"), 
+                                           selected = "Spring"))),
              
              column(3, htmlOutput("userInputAll"))
     )
