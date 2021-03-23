79f7e1cfad664fbd8f6c91e2ac728329fac2da82
uedah1999 <euro.japonais@gmail.com>
2021-03-23 15:30:44 -0500

    add comment and use the words subject and division

--- module_bar.R
+++ module_bar.R
@@ -1,37 +1,28 @@
+# Written by Hiromichi Ueda '21 (DataSquad) in March 2021
+# Requirement: R version 4.0, with installation of packages tidyverse and ggplot2
+# Last executed in March 2021
+
 library(tidyverse)
 library(ggplot2)
 
-get_subject_str <- function(course_shortname) {
-  if (tolower(substr(course_shortname, start=1, stop=2)) %in% c('cs', 'pe')) {
+# given a course shortname, returns abbreviation of subject
+# e.g.) given 'arbc103-01-s20', returns arbc
+get_subject <- function(course_shortname, division_lookup) {
+  if (tolower(substr(course_shortname, start=1, stop=4)) %in% division_lookup$subject) {
+    subj_str <- tolower(substr(course_shortname, start=1, stop=4))
+  } else if (tolower(substr(course_shortname, start=1, stop=2)) %in% c('cs', 'pe')) {
     subj_str <- tolower(substr(course_shortname, start=1, stop=2))
   } else if (tolower(substr(course_shortname, start=1, stop=3)) %in% c('grk', 'gsc')){
     subj_str <- tolower(substr(course_shortname, start=1, stop=3))
   } else if (tolower(substr(course_shortname, start=1, stop=5)) == 'jalli'){
     subj_str <- tolower(substr(course_shortname, start=1, stop=5))
   } else {
-    subj_str <- tolower(substr(course_shortname, start=1, stop=4))
+    subj_str <- 'other'
   }
   return(subj_str)
 }
 
-get_discipline <- function(course_shortname, division_lookup) {
-  subj_str <- get_subject_str(course_shortname)
-  if (subj_str %in% division_lookup$subject) {
-    return(division_lookup[division_lookup$subject==subj_str, ]$division)
-  } else {
-    return('Other')
-  }
-}
-
-get_subject <- function(course_shortname, division_lookup) {
-  subj_str <- get_subject_str(course_shortname)
-  if (subj_str %in% division_lookup$subject) {
-    return(division_lookup[division_lookup$subject==subj_str, ]$fullname)
-  } else {
-    return('Other')
-  }
-}
-
+# returns title of the bar chart
 get_title <- function(term_str, division, subject) {
   yr_str <- str_sub(term_str, 1, 4)
   season_str <- str_sub(term_str, start=5)
@@ -51,7 +42,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path ))
 # term must be between F14-S16 or F19-F20
 plot_counts <- function(term, 
                         div = 'All',
-                        subj = 'All',
+                        subj_full = 'All',
                         division_path = './sb21_division_lookup.csv', 
                         interactive_lookup = c("assign", "chat", "database", "feedback", "forum", 
                                                "glossary", "languagelesson", "quiz", "survey", "workshop")) {
@@ -65,31 +56,32 @@ plot_counts <- function(term,
     left_join(read_csv(rest_path), by="shortname") %>%
     select(-category)
   
-  # add columns subject (Arabic) and discipline (Arts & Literature)
+  # add columns subject (arbc), fullname (Arabic) and division (Arts & Literature)
   df_division <- read_csv(division_path)
-  df$subject <- lapply(df$shortname, function(x) sapply(x, get_subject, division_lookup=df_division))
-  df$discipline <- lapply(df$shortname, function(x) sapply(x, get_discipline, division_lookup=df_division))
+  df <- df %>% 
+    mutate(subject = sapply(shortname, get_subject, division_lookup=df_division)) %>%
+    left_join(df_division, by='subject')
   
-  # if specific discipline/division is selected, then we should limit the choice for subject
-  # e.g. if Humanities is selected, then math cannot be chosen as the subject
-  if (subj != 'All') {
-    df <- filter(df, subject==subj)
+  if (subj_full != 'All') {
+    df <- filter(df, fullname==subj_full)
   }
   if (div != 'All') {
-    df <- filter(df, discipline==div)
+    df <- filter(df, division==div)
   }
   
+  # df has 'shortname', 21 columns about '_count', then 'subject', 'fullname', 'division'
+  # e.g.) arbc103-01-s20, <all the counts>, arbc, Arabic, Arts & Literature
   df_count <- data.frame(
-    module = substr(colnames(df[,c(-1,-22,-23)]), start=1, stop=nchar(colnames(df[,c(-1,-22,-23)]))-6),
+    module = substr(colnames(df[,c(-1,-22,-23,-24)]), start=1, stop=nchar(colnames(df[,c(-1,-22,-23,-24)]))-6),
     count = colSums(Filter(is.numeric, df))
   ) %>% 
     mutate(is_interactive = module %in% interactive_lookup)
   
   ggplot(df_count) + 
     geom_bar(aes(x = module, y=count, fill=is_interactive), stat='identity') + 
-    ggtitle(get_title(term, div, subj))
+    ggtitle(get_title(term, div, subj_full))
 }
 
 division_type <- c("Arts & Literature", "Humanities", "Interdisciplinary Studies", 
                    "Natural Sciences & Mathematics", "Other", "PEAR", "Social Sciences" )
-plot_counts(term = '2020Spring', div = 'All', subj = 'Mathematics')
+plot_counts(term = '2020Spring', div = 'All', subj_full = 'Mathematics')
