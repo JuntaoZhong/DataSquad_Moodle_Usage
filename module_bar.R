dd229610b153d0d2f654f53d16afd9e7456eba67
uedah1999 <euro.japonais@gmail.com>
2021-04-04 11:07:51 -0500

    modify to take multiple divisions and subjects as input, and to plot average count

--- module_bar.R
+++ module_bar.R
@@ -26,10 +26,10 @@ get_subject <- function(course_shortname, division_lookup) {
 get_title <- function(term_str, division, subject) {
   yr_str <- str_sub(term_str, 1, 4)
   season_str <- str_sub(term_str, start=5)
-  if (subject != 'All') {
-    return(paste0(season_str, ' ', yr_str, ', Subject: ', subject))
-  } else if (division != 'All') {
-    return(paste0(season_str, ' ', yr_str, ', Division: ', division))
+  if (!'All' %in% subject) {
+    return(paste0(season_str, ' ', yr_str, ', Subject: ', paste(subj_full, collapse = ', ')))
+  } else if (!'All' %in% division) {
+    return(paste0(season_str, ' ', yr_str, ', Division: ', paste(division, collapse = ', ')))
   } else {
     return(paste0(season_str, ' ', yr_str))
   }
@@ -41,20 +41,21 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path ))
 
 # term must be between F14-S16 or F19-F20
 plot_counts <- function(term, 
-                        div = 'All',
-                        subj_full = 'All',
+                        div = c('All'),
+                        subj_full = c('All'),
                         division_path = './sb21_division_lookup.csv', 
                         interactive_lookup = c("assign", "chat", "database", "feedback", "forum", 
                                                "glossary", "languagelesson", "quiz", "survey", "workshop")) {
   # load files from the specified term
-  puf_path <- paste0("./ModuleCounts/Mdata_", term, "_ModuleCounts_PagesUrlsFiles.csv")
-  rest_path <- paste0("./ModuleCounts/Mdata_", term, "_ModuleCounts_rest.csv")
+  puf_path <- paste0("../ModuleCounts/Mdata_", term, "_ModuleCounts_PagesUrlsFiles.csv")
+  rest_path <- paste0("../ModuleCounts/Mdata_", term, "_ModuleCounts_rest.csv")
   
   # import the ModuleCounts csv files
   df <- read_csv(puf_path) %>%
     select(-category) %>% 
     left_join(read_csv(rest_path), by="shortname") %>%
     select(-category)
+  colnames(df) <- c(c('shortname'), str_sub(colnames(df)[-1], start = 1, end=-7)) # remove _count from column names
   
   # add columns subject (arbc), fullname (Arabic) and division (Arts & Literature)
   df_division <- read_csv(division_path)
@@ -62,26 +63,27 @@ plot_counts <- function(term,
     mutate(subject = sapply(shortname, get_subject, division_lookup=df_division)) %>%
     left_join(df_division, by='subject')
   
-  if (subj_full != 'All') {
-    df <- filter(df, fullname==subj_full)
+  if (!'All' %in% subj_full) {
+    df <- filter(df, fullname %in% subj_full)
   }
-  if (div != 'All') {
+  if (!'All' %in% div) {
     df <- filter(df, division==div)
   }
   
   # df has 'shortname', 21 columns about '_count', then 'subject', 'fullname', 'division'
   # e.g.) arbc103-01-s20, <all the counts>, arbc, Arabic, Arts & Literature
   df_count <- data.frame(
-    module = substr(colnames(df[,c(-1,-22,-23,-24)]), start=1, stop=nchar(colnames(df[,c(-1,-22,-23,-24)]))-6),
-    count = colSums(Filter(is.numeric, df))
-  ) %>% 
+    module = colnames(Filter(is.numeric, df)),
+    avg_count = df %>% summarise_if(is.numeric, mean) %>% t()
+  ) %>%
+    remove_rownames() %>%
     mutate(is_interactive = module %in% interactive_lookup)
   
   ggplot(df_count) + 
-    geom_bar(aes(x = module, y=count, fill=is_interactive), stat='identity') + 
+    geom_bar(aes(x = module, y=avg_count, fill=is_interactive), stat='identity') + 
     ggtitle(get_title(term, div, subj_full))
 }
 
 division_type <- c("Arts & Literature", "Humanities", "Interdisciplinary Studies", 
                    "Natural Sciences & Mathematics", "Other", "PEAR", "Social Sciences" )
-plot_counts(term = '2020Spring', div = 'All', subj_full = 'Mathematics')
+plot_counts(term = '2020Fall', div = c('All'), subj_full = c('All'))#= c('Mathematics', 'Statistics'))
