cc1152201c90f1fd69539fc34016e849078680d1
uedah1999 <euro.japonais@gmail.com>
2021-04-18 13:19:50 -0500

    modify to use a single csv file; modify terms_str of plot_counts to take multiple terms; add interactive plot of across-term usage

--- module_bar.R
+++ module_bar.R
@@ -1,9 +1,12 @@
-# Written by Hiromichi Ueda '21 (DataSquad) in March 2021
-# Requirement: R version 4.0, with installation of packages tidyverse and ggplot2
-# Last executed in March 2021
+# Written by Hiromichi Ueda '21 (DataSquad) in April 2021
+# Requirement: R version 4.0, with installation of the following packages:
+# tidyverse, ggplot2, plotly, Dict
+# Last executed in April 2021
 
 library(tidyverse)
 library(ggplot2)
+library(plotly)
+library(Dict)
 
 # returns title of the bar chart
 get_title <- function(term_str, division, subject) {
@@ -18,28 +21,26 @@ get_title <- function(term_str, division, subject) {
   }
 }
 
-# set working directory 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path ))
-
-
-# term must be between F14-S16 or F19-F20
-plot_counts <- function(term_str, 
-                        div = c('All'),
-                        subj_full = c('All'),
-                        division_path = './sb21_division_lookup.csv', 
-                        interactive_lookup = c("assign", "chat", "database", "feedback", "forum", 
-                                               "glossary", "languagelesson", "quiz", "survey", "workshop")) {
-  # load the file with data for all terms
-  df <- read_csv("../ModuleCounts/Mdata_all.csv") %>%
-    filter(term == term_str)
-  
-  if (!'All' %in% subj_full) {
-    df <- filter(df, fullname %in% subj_full)
+# sort academic terms in chronological order (WI < SP < FA)
+sort_terms <- function(all_terms) {
+  term_month_dict <- Dict$new(
+    Fall = '09',
+    Winter = '01',
+    Spring = '04'
+  ) # starting month
+  all_terms_num <- rep_len(0, length(all_terms))
+  for (i in 1:length(all_terms)) {
+    all_terms_num[i] <- paste0(str_sub(all_terms[i], 1, 4), term_month_dict[str_sub(all_terms[i], 5)])
   }
-  if (!'All' %in% div) {
-    df <- filter(df, division==div)
+  terms_order_idx <- order(all_terms_num) # index of sorted elements
+  terms_sorted <- rep_len('term', length(all_terms))
+  for (i in 1:length(all_terms)) {
+    terms_sorted[i] <- all_terms[terms_order_idx[i]]
   }
-  
+  return(terms_sorted)
+}
+
+plot_counts_oneterm <- function(df, term_str, div, subj_full, interactive_lookup) {
   # df has 'shortname', 21 columns about '_count', then 'subject', 'fullname', 'division'
   # e.g.) arbc103-01-s20, <all the counts>, arbc, Arabic, Arts & Literature
   df_count <- data.frame(
@@ -53,15 +54,45 @@ plot_counts <- function(term_str,
     geom_bar(aes(x = module, y=avg_count, fill=is_interactive), stat='identity') + 
     ggtitle(get_title(term_str, div, subj_full))
 }
+  
+plot_counts_crossterm <- function(df, terms_str, div, subj_full, interactive_lookup) {
+  p <- aggregate(. ~ term, df[-c(2:5)], mean) %>% # average counts; drop admin info about course
+    gather(module, count, assign:workshop, factor_key = TRUE) %>% # convert to long format
+    mutate(term = factor(term, levels = sort_terms(unique(df$term)))) %>% # sort terms (WI < SP < FA)
+    ggplot(aes(x = term, y = count, group=module, color=module)) +
+    geom_point(size=1) +
+    geom_line()
+  ggplotly(p, tooltip = c('x', 'y', 'group'))
+}
+
+# set working directory 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path ))
+
+
+# term must be between F14-S16 or F19-F20
+plot_counts <- function(terms_str, 
+                        div = c('All'),
+                        subj_full = c('All'),
+                        division_path = './sb21_division_lookup.csv', 
+                        interactive_lookup = c("assign", "chat", "database", "feedback", "forum", 
+                                               "glossary", "languagelesson", "quiz", "survey", "workshop")) {
+  df <- read_csv("../ModuleCounts/Mdata_all.csv")
+  if (!'All' %in% terms_str) {
+    df <- filter(df, term %in% terms_str)
+  }
+  if (!'All' %in% subj_full) {
+    df <- filter(df, fullname %in% subj_full)
+  }
+  if (!'All' %in% div) {
+    df <- filter(df, division==div)
+  }
+  if ((!'All' %in% terms_str) & (length(terms_str) == 1)) {
+    plot_counts_oneterm(df, terms_str[1], div, subj_full, interactive_lookup)
+  } else {
+    plot_counts_crossterm(df, terms_str, div, subj_full, interactive_lookup)
+  }
+}
 
 division_type <- c("Arts & Literature", "Humanities", "Interdisciplinary Studies", 
                    "Natural Sciences & Mathematics", "Other", "PEAR", "Social Sciences" )
-plot_counts(term_str = '2020Fall', div = c('All'), subj_full = c('All'))#= c('Mathematics', 'Statistics'))
-
-df <- read_csv("../ModuleCounts/Mdata_all.csv")
-p <- aggregate(. ~ term, df[-c(2:5)], mean) %>% # drop administrative info about course
-  gather(module, count, assign:workshop, factor_key = TRUE) %>%  # convert to long
-  ggplot(aes(x = term, y = count, group=module, color=module)) +
-    geom_point(size=1) +
-    geom_line()
-p
+plot_counts(terms_str = c('All'), div = c('All'), subj_full = c('All'))
